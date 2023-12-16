cdc745794d36f1da03b0960eb0b932fa849f475a
Saulompr <saulompr@gmail.com>
2023-12-16 19:21:57 +0000

    Enemy L movement

--- Assets/Scripts/MoveInLoop.cs
+++ Assets/Scripts/MoveInLoop.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class MoveInLoop : MonoBehaviour
 {
     public Transform startPoint;
+    public Transform middlePoint;
     public Transform endPoint;
     public float speed = 0.5f;
 
@@ -17,18 +18,17 @@ public class MoveInLoop : MonoBehaviour
 
     IEnumerator Patroll () {
         while (true) {
-            
-            //IR al destino
-            Vector3 towardsTarget = endPoint.position - transform.position;
+            //IR al medio
+            Vector3 towardsTarget = middlePoint.position - transform.position;
             while (towardsTarget.magnitude > 0.1f) {
                 transform.position += towardsTarget.normalized * speed * Time.deltaTime;
                 yield return 0;
-                towardsTarget = endPoint.position - transform.position;
+                towardsTarget = middlePoint.position - transform.position;
             }
 
-            //Dar media vuelta
+            //girar al final
             Quaternion startRotation = transform.rotation;
-            Quaternion rotationTarget = startRotation * Quaternion.Euler(0,180,0);
+            Quaternion rotationTarget = startRotation * Quaternion.Euler(0,90,0);
             float elapsedTime = 0;
             float animationTime = 0.5f;
             while (elapsedTime < animationTime) {
@@ -36,7 +36,45 @@ public class MoveInLoop : MonoBehaviour
                 elapsedTime += Time.deltaTime;
                 yield return 0;
             }
+
+            //IR al final
+            towardsTarget = endPoint.position - transform.position;
+            while (towardsTarget.magnitude > 0.1f) {
+                transform.position += towardsTarget.normalized * speed * Time.deltaTime;
+                yield return 0;
+                towardsTarget = endPoint.position - transform.position;
+            }
+
+            //Dar media vuelta
+            startRotation = transform.rotation;
+            rotationTarget = startRotation * Quaternion.Euler(0,180,0);
+            elapsedTime = 0;
+            animationTime = 1f;
+            while (elapsedTime < animationTime) {
+                transform.rotation = Quaternion.Lerp(startRotation, rotationTarget, elapsedTime / animationTime);
+                elapsedTime += Time.deltaTime;
+                yield return 0;
+            }
+
+            //IR al medio
+            towardsTarget = middlePoint.position - transform.position;
+            while (towardsTarget.magnitude > 0.1f) {
+                transform.position += towardsTarget.normalized * speed * Time.deltaTime;
+                yield return 0;
+                towardsTarget = middlePoint.position - transform.position;
+            }
             
+            //girar al principio
+            startRotation = transform.rotation;
+            rotationTarget = startRotation * Quaternion.Euler(0,-90,0);
+            elapsedTime = 0;
+            animationTime = 0.5f;
+            while (elapsedTime < animationTime) {
+                transform.rotation = Quaternion.Lerp(startRotation, rotationTarget, elapsedTime / animationTime);
+                elapsedTime += Time.deltaTime;
+                yield return 0;
+            }
+
             //Volver al origen
             towardsTarget = startPoint.position - transform.position;
             while (towardsTarget.magnitude > 0.1f) {
@@ -44,11 +82,12 @@ public class MoveInLoop : MonoBehaviour
                 yield return 0;
                 towardsTarget = startPoint.position - transform.position;
             }
-            
+
             //Dar media vuelta
             startRotation = transform.rotation;
-            rotationTarget = startRotation * Quaternion.Euler(0,180,0);
+            rotationTarget = startRotation * Quaternion.Euler(0,-180,0);
             elapsedTime = 0;
+            animationTime = 1f;
             while (elapsedTime < animationTime) {
                 transform.rotation = Quaternion.Lerp(startRotation, rotationTarget, elapsedTime / animationTime);
                 elapsedTime += Time.deltaTime;
