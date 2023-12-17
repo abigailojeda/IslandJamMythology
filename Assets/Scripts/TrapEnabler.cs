cbbc9b24eb2c99bb032ab7b66eb411f1e12f84bb
GG-Brian <71889035+GG-Brian@users.noreply.github.com>
2023-12-17 09:22:44 +0000

    trapEnabler.cs upgrades, trap.cs unneeded

--- Assets/Scripts/TrapEnabler.cs
+++ Assets/Scripts/TrapEnabler.cs
@@ -2,50 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapSwitch : MonoBehaviour {
+public class TrapEnabler : MonoBehaviour {
 
     public GameObject trap;
-    public int pushSide; // 1 xup 2 xdown, 3 zup, 4 zdown
-    private Vector3 trapPositionInitial;
-    private Vector3 trapPositionActive;
-    private bool isActive;
-    private bool isMoving;
+    public float trapX, trapZ;
 
-    // Start is called before the first frame update
-    //void Start() {
-    //    isActive = false;
-    //}
+    private Vector3 trapPositionInitial;
+    private bool isPressed;
+
+    private void Awake() {
+        trapPositionInitial = trap.transform.position;
+    }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Player")) {
-            //if (isActive == false) {
-            //    //Audio.instance.PlaySFX("TrapSpikesDelay");
-            //    if (pushSide == 1) { transform.position = new Vector3(transform.position.x + 4f, transform.position.y, transform.position.z); }
-            //    else if (pushSide == 2) { transform.position = new Vector3(transform.position.x - 4f, transform.position.y, transform.position.z); }
-            //    else if (pushSide == 3) { transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 4f); }
-            //    else { transform.position = new Vector3(transform.position.x, transform.position.y - 2.0f, transform.position.z - 4f); }
-
-            //    Invoke("activate", 1);
-            //}
-
-            trap.transform.position = new Vector3(transform.position.x - 4f, transform.position.y, transform.position.z);
-            Invoke("activate", 1);
+            if (isPressed == false) {
+                //Audio.instance.PlaySFX("TrapSpikesDelay");
+                Invoke("activate", 1);
+                isPressed = true;
+            }
         }
     }
 
     public void activate() {
-        Debug.Log("activation happened");
+        //Audio.instance.PlaySFX("TrapSpikesActive");
+        trap.transform.position = new Vector3(trapX, trap.transform.position.y, trapZ);
         Invoke("deactivate", 3);
     }
 
-    private void deactivate() {
-        Debug.Log("biebie");
-        //if (pushSide == 1) { transform.position = new Vector3(transform.position.x - 4f, transform.position.y, transform.position.z); }
-        //else if (pushSide == 2) { transform.position = new Vector3(transform.position.x + 4f, transform.position.y, transform.position.z); }
-        //else if (pushSide == 3) { transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 4f); }
-        //else { transform.position = new Vector3(transform.position.x, transform.position.y - 2.0f, transform.position.z + 4f); }
-
-        trap.transform.position = new Vector3(transform.position.x + 4f, transform.position.y, transform.position.z);
-        isActive = false;
+    public void deactivate() {
+        //Audio.instance.PlaySFX("TrapSpikesDeactive");
+        trap.transform.position = trapPositionInitial;
+        isPressed = false;
     }
 }
